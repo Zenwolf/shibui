157e9def6f6f25df57ae18cf236e16296fde199b
Matthew Jaquish <mattjaq@gmail.com>
2013-12-14 00:56:11 -0800

    Added license.

---
+++ src/Interface.js
@@ -0,0 +1,5 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Copyright 2012, 2013 Matthew Jaquish
+Licensed under the Apache License, Version 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
